341870bca7c797857dad3de0e9a748298a5c7475
Alexander Spanos <alexisspa9@gmail.com>
2018-03-27 10:17:55 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,1 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 3, "Female", "m", "brown", "funny", true)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr.Chocolate", 3, "Female", "m", "brown", "funny", True)
